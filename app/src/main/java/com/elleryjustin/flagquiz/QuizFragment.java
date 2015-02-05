65ae9a782481fa527b8d49584acc98bde572a00a
Justin <justin_ellery@hotmail.ca>
2015-02-04 23:42:36 -0500

    Working on fragment

---
+++ app/src/main/java/com/elleryjustin/flagquiz/QuizFragment.java
@@ -0,0 +1,11 @@
+package com.elleryjustin.flagquiz;
+
+import android.app.Fragment;
+
+/**
+ * Created by Justin on 2015-02-04.
+ */
+public class QuizFragment extends Fragment
+{
+
+}
---
+++ app/src/main/java/com/elleryjustin/flagquiz/SettingsFragment.java
@@ -0,0 +1,17 @@
+package com.elleryjustin.flagquiz;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by Justin on 2015-02-04.
+ */
+public class SettingsFragment extends PreferenceFragment
+{
+    @Override
+    public void onCreate (Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+}
